5110cf342717983f0ebb38c5c23b59b9d5e50c92
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-10-22 22:22:37 +0200

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { Login, Register } from './pages';
+
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/login',
+      element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+    },
+  ]);
   return (
     <div>
-      {/* <Login /> */}
-      <Register />
+      <RouterProvider router={router} />
     </div>
   );
 }
