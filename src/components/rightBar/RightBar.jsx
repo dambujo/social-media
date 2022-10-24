b56a540ccb6aef78ffc16ba6c9022c8f3c4c847d
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-10-24 20:41:23 +0200

    Right Menu Bar UI Design

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -2,7 +2,169 @@ import React from 'react';
 import './RightBar.scss';
 
 const RightBar = () => {
-  return <div>RightBar</div>;
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <p>
+                <span> Jane Doe </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <p>
+                <span> Jane Doe </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span>2 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <p>
+                <span> Jane Doe </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span>3 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <p>
+                <span> Jane Doe </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span>4 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Frineds</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/photos/what-challenges-us-can-help-change-us-picture-id1014076102?k=20&m=1014076102&s=612x612&w=0&h=tcCHSV1TF-3KWj2mH4GVd4CxQdJJabhd2Wit0kLok5U="
+                alt="Jane"
+              />
+              <div className="online" />
+              <span> Jane Doe </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightBar;
